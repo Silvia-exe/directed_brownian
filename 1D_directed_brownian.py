3c1e906c2a6e8580b5fa14f8da9055e767593901
Silvia <silmor1999@gmail.com>
2025-04-24 16:22:19 +0200

    Initial files, 1D 90% done, 2D placeholder

---
+++ 1D_directed_brownian.py
@@ -0,0 +1,93 @@
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+
+#1D directed random walk of N particles
+
+#Implements reflectibe boundary conditions.
+def reflective_bc(delta):
+    if v0 == 0:
+        return 0
+
+    diff = abs(delta)//v0
+    delta += -1*np.sign(delta)*abs(diff)*v0 #Periodic boundary conditions
+    delta *= 1-2*(abs(diff)%2)
+    return delta
+
+#Updates the position of the particle
+def update_r(r_old, del_old):
+    deli = np.array([reflective_bc(d + v1*rnd.uniform(-1,1)) for d in del_old])
+    ri = np.array([r_old[i] + deli[i]*dt for i in range(len(del_old))])
+    return (ri, deli)
+
+
+T = 12**3
+dt = 0.5
+N = 11**3
+
+v0 = 10.0 #Directed velocity, will describe self-propelled motion
+tau_v = [1,2,5,100,400,1600,6400,25600]
+
+for tau in tau_v:
+    v1 = (v0*dt)/tau #Random drift velocity, amplitude of random displacement
+
+    r0 = np.zeros(N)
+    del0= np.array([reflective_bc(rnd.uniform(-1,1)*v0) for _ in range(N)])
+
+    T_v = np.arange(0, T,step = dt)
+    pos_v = np.array([r0]) #Positions of particles
+    del_v = np.array([del0]) #Ramdon fluctuations of particles
+    #true_brownian = [r0] #True random brownian motion (DEBUG)
+    posdiff_v = np.array([r0]) #For MSD WIP
+
+    r_old = r0
+    del_old = del0
+
+    if N > 1:
+        for t in range(len(T_v)-1):
+            r_old, del_old = update_r(r_old, del_old)
+            pos_v = np.append(pos_v, [r_old], axis = 0)
+            del_v = np.append(del_v, [del_old], axis = 0)
+            posdiff_v = np.append(posdiff_v, [(r0 - r_old)**2], axis = 0)
+
+        msd_v = np.average(posdiff_v, axis = 1, keepdims= True) #MSD of particle ensemble
+        plt.plot(msd_v, label = "$𝜏 = $ " + str(tau))
+
+    else:
+        for t in range(len(T_v)-1):
+            r_old, del_old = update_r(r_old, del_old)
+            pos_v = np.append(pos_v, [r_old], axis = 0)
+            del_v = np.append(del_v, [del_old], axis = 0)
+
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("Time [t]")
+plt.legend()
+plt.ylabel("MSD [$x^2$] (Ensemble average)")
+plt.title("Mean Square Displacement")
+plt.show()
+
+'''r_old = r0
+for i in range(0,T):
+    r_old += rnd.uniform(-1,1)*v1
+    true_brownian.append(r_old)
+plt.plot(true_brownian, marker='.')
+plt.title("true_brownian")
+plt.xlabel("Time [t]")
+plt.ylabel("Position 1D [x]")
+plt.show()'''
+
+'''for p in range(N):
+    plt.plot(T_v, pos_v[:, p])
+#plt.axline((0,r0), slope = del0, color = "red", lw = 1, label ="Direction of dir. movement.")
+plt.xlabel("Time [t]")
+plt.ylabel("Position 1D [x]")
+#plt.legend()
+plt.title("1D directed Brownian motion of "+ str(N) + " particles")
+plt.show()'''
+
+'''plt.plot(T_v, del_v[:, 0], marker='.')
+plt.xlabel("Time [t]")
+plt.ylabel("$\delta(t)$ [x]")
+plt.title("$\delta(t)$")
+plt.show()'''
